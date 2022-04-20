5adc6e500334641270feab3d886ed7a33e6426b5
Stefan Mandel <mandelst@gmail.com>
2022-04-20 13:06:48 +0200

    refine examples for methods

--- src/main/java/de/andrena/readablecode/stefan/methods/b/Pricing.java
+++ src/main/java/de/andrena/readablecode/stefan/methods/b/Pricing.java
@@ -4,13 +4,23 @@ import static de.andrena.readablecode.Use.use;
 
 public class Pricing {
 	private TaxingProperties taxingProperties;
+	private CustomerProperties customerProperties;
 
-	public Pricing(TaxingProperties taxingProperties) {
+	public Pricing(TaxingProperties taxingProperties, CustomerProperties customerProperties) {
 		this.taxingProperties = taxingProperties;
+		this.customerProperties = customerProperties;
 	}
 
-	public Price computeTaxes(Price price, ItemProperties itemProperties, CustomerProperties customerProperties) {
+	/**
+	 * Magic Complexity for client
+	 * - uses this object        + 1
+	 * - computes Taxes          + 1
+	 * - uses price              + 1
+	 * - uses item Properties    + 1
+	 */
+	public Price computeTaxes(Price price, ItemProperties itemProperties) {
 		use(taxingProperties);
+		use(customerProperties);
 		return null;
 	}
 }
