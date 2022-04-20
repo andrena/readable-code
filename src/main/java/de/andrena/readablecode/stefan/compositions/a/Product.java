62773e1cda055107eb8fe9cbb0cccf6fd84c4c2b
Stefan Mandel <mandelst@gmail.com>
2022-04-20 15:34:24 +0200

    examples for structs

---
+++ src/main/java/de/andrena/readablecode/stefan/compositions/a/Product.java
@@ -0,0 +1,17 @@
+package de.andrena.readablecode.stefan.compositions.a;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class Product {
+	/** Magic Complexity =             0 */
+	public int id;                   //+1
+	public String title;             //+1
+	public String description;       //+1
+	public BigDecimal price;         //+1
+	public int available;            //+1
+	public List<String> comments;    //+1
+	public List<Integer> stars;      //+1
+	/**                                4 */
+
+}
