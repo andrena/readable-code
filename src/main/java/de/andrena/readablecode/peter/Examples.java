4fa4041f80458efab54abf438a7abab489e9328d
Peter Guntermann <pgu@posteo.de>
2022-04-20 11:19:46 +0200

    Initial commit Peter's examples

---
+++ src/main/java/de/andrena/readablecode/peter/Examples.java
@@ -0,0 +1,14 @@
+package de.andrena.readablecode.peter;
+
+import java.util.List;
+
+public class Examples {
+
+    private List list;
+    private List fruitList;
+    private List lemonFruitList;
+    private List listOfLemonsLimesOrangesMandarinsGrapefruitsPomelos;
+
+
+
+}
