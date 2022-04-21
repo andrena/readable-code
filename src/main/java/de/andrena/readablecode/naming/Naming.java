30f0aa4955d60f4c3f2c0531b706c8cb8b498020
Stefan Mandel <Stefan.Mandel@andrena.de>
2022-04-21 14:30:06 +0200

    rework examples

--- src/main/java/de/andrena/readablecode/naming/Naming.java
+++ src/main/java/de/andrena/readablecode/naming/Naming.java
@@ -26,16 +26,46 @@ public class Naming {
 	}
 	
 	public static class InTemporaryVariables {
-		public void method() {
-			/** six chunks, expressive but not recallable */
-			String propertySubStringAfterSecondDot = null; // do some research how this one was used
-			/** one chunk, less expressive but recallable */
-			String suffix = null; 
+		
+		public String getViolationElement1(String propertyPath) {
+	        String[] splitProperty = propertyPath.split("\\.", 3);
+	        /** precise but not recallable */
+	        String propertySubStringAfterSecondDot = splitProperty[splitProperty.length - 1];
+	        return propertySubStringAfterSecondDot;
+	    }
+
+		public String getViolationElement2(String propertyPath) {
+			/** without variables, describing the whole process */
+			return new StringExtractor(propertyPath)
+				.consumeUntil('.')
+				.consumeUntil('.')
+				.remainder();
+	    }
+	}
+	
+	public static class StringExtractor {
+
+		private String remainder;
+
+		public StringExtractor(String remainder) {
+			this.remainder = remainder;
 		}
+
+		public StringExtractor consumeUntil(char c) {
+			int pos = remainder.indexOf(c); 
+			remainder = remainder.substring(pos+1);
+			return this;
+		}
+
+		public String remainder() {
+			return remainder;
+		}
+		
 	}
 	
 	public static class ProxySelectorAdapter {
 	}
+	
 	/** 7-8 chunks = not readable*/
 	public static class RetryOnceProxyAddressToProxySelectorAdapter extends ProxySelectorAdapter {
 		
