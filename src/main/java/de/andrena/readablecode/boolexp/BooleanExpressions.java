d028409d5f5a3afe11f2a5b2a76524eb920f1a6a
Stefan Mandel <mandelst@gmail.com>
2022-04-21 18:53:10 +0200

    code examples for boolean/set theory

---
+++ src/main/java/de/andrena/readablecode/boolexp/BooleanExpressions.java
@@ -0,0 +1,25 @@
+package de.andrena.readablecode.boolexp;
+
+public class BooleanExpressions {
+
+	public static void main(String[] args) {
+		System.out.println("\n1 chunk = ok");
+		System.out.println("------------");
+		new Formula((boolean a) -> true, "true").evaluate(true);
+		new Formula((boolean a) -> a, "A").evaluate(true);
+
+		System.out.println("\n3 chunks = ok");
+		System.out.println("-------------");
+		new Formula((a, b) -> a & b, "A & B").evaluate(true, false);
+
+		System.out.println("\n4 chunks = ok");
+		System.out.println("-------------");
+		new Formula((a, b) -> a & !b, "A & !B").evaluate(true, false);
+		new Formula((a, b) -> !a | b, "!A | B").evaluate(true, false);
+
+		System.out.println("\n6 chunks = hard to recall");
+		System.out.println("-------------");
+		new Formula((a, b, c) -> a & b | !c, "A & B | !C").evaluate(true, false, false);
+	}
+
+}
---
+++ src/main/java/de/andrena/readablecode/boolexp/SetFormula.java
@@ -0,0 +1,64 @@
+package de.andrena.readablecode.boolexp;
+
+import java.util.Set;
+import java.util.function.Function;
+
+public class SetFormula {
+
+	private static String[] VARIABLES = {"A","B","C"};
+	
+	private Function<Set<String>[],Set<String>> formula;
+	private String toString;
+
+	public SetFormula(P1 formula, String toString) {
+		this((Set<String>[] a) -> formula.test(a[0]), toString);
+	}
+
+	public SetFormula(P2 formula, String toString) {
+		this((Set<String>[] a) -> formula.test(a[0], a[1]), toString);
+	}
+	
+	public SetFormula(P3 formula, String toString) {
+		this((Set<String>[] a) -> formula.test(a[0], a[1],a[2]), toString);
+	}
+	
+	public SetFormula(Function<Set<String>[],Set<String>> formula, String toString) {
+		this.formula = formula;
+		this.toString = toString;
+	}
+	
+	@SafeVarargs
+	public final void evaluate(Set<String>... arguments) {
+		int i = 0;
+		for (Set<String> arg : arguments) {
+			System.out.println(VARIABLES[i] + " = " + arg);
+			i++;
+		}
+		System.out.print("\t" + toString + " = ");
+		Set<String> test = formula.apply(arguments);
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
+		System.out.println(test);
+	}
+
+	public interface P1 {
+
+		Set<String> test(Set<String> a);
+
+	}
+
+	public interface P2 {
+
+		Set<String> test(Set<String> a, Set<String> b);
+
+	}
+
+	public interface P3 {
+
+		Set<String> test(Set<String> a, Set<String> b, Set<String> c);
+
+	}
+}
