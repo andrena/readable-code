827525990f8cd7bc0a3d4187d152ed823aa5556c
Stefan Mandel <Stefan.Mandel@andrena.de>
2022-04-25 09:13:56 +0200

    UTF-8

--- src/main/java/de/andrena/readablecode/chunking/PrimitiveChunking.java
+++ src/main/java/de/andrena/readablecode/chunking/PrimitiveChunking.java
@@ -7,8 +7,8 @@ public class PrimitiveChunking {
 	public static final String B_NUMBER = "0163-89 77 42";      //4 chunks, we use telephone number idiom, recallable
 
 	public static final String CHARACTERS ="CFEG";              //4 chunks, recallable
-	public static final String MANY_CHARACTERS = "GNUHCSARREBÜ";//12 chunks, not recallable
-	public static final String SAME_CHARACTERS = "ÜBERRASCHUNG";//1 chunk, we use german language, recallable
+	public static final String MANY_CHARACTERS = "GNUHCSARREBÃœ";//12 chunks, not recallable
+	public static final String SAME_CHARACTERS = "ÃœBERRASCHUNG";//1 chunk, we use german language, recallable
 	
 	/**
      * 13 chunks?
